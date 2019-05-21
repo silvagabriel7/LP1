f88a157c53061dc040fee8f485bb42370a07e52e
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-05-21 15:41:33 -0300

    Update 7.15.C

--- Exercicios/Compostos/7.15.C
+++ Exercicios/Compostos/7.15.C
@@ -1,67 +1,73 @@
 
 #include <stdio.h>
 
-struct animais{
-    int sub;
-    union animal{
-        struct anfibios{
-            int patas;
-            double esp_pele;
-            int cauda;
-        };
-        struct aves{
-            double tam_bico;
-            double auto_voo;
-            double frenq_canto;
-        };
-        struct mamiferos{
-            int dentes;
-            double vol_cereb;
-            double velocidade;
-        };
-        struct repteis{
-            int venenoso;
-            double auto_agua;
-            double esp_ovos;
-        };
-    };
+struct anfibios{
+    int patas;
+    double esp_pele;
+    int cauda;
+};
+struct aves{
+    double tam_bico;
+    double auto_voo;
+    double frenq_canto;
+};
+struct mamiferos{
+    int dentes;
+    double vol_cereb;
+    double velocidade;
+};
+struct repteis{
+    int venenoso;
+    double auto_agua;
+    double esp_ovos;
 };
 
-void preenche(struct animais* a){
+struct animais{
+    int sub;
+    union{
+        struct anfibios novo_anfibio;
+        struct aves novo_ave;
+        struct mamiferos novo_mamifero;
+        struct repteis novo_reptil;
+    }a;
+};
+
+void preenche(struct animais* x){
     printf("Anfibios[1]\nAves[2]\nMamíferos[3]\nRépteis[4]\nEscolha qual classe de animal deseja preencher: \n");
-    scanf("%d",a->sub);
-    switch(a->sub){
+    scanf("%d",&x->sub);
+    switch(x->sub){
         case 1:
-            printf("ANFIBIOS");
+            printf("ANFIBIOS\n");
         break;
         case 2:
-            printf("AVES");
+            printf("AVES\n");
         break;
         case 3:
-            printf("MAMIFEROS");
+            printf("MAMIFEROS\n");
         break;
         case 4:
-            printf("REPTEIS");
+            printf("REPTEIS\n");
         break;
+        default:
+            printf("ERRO\n");
     }
         
 }
 
 
-
 int main(void){
+    int i;
     struct animais novo_animal1;
-    exibe(&novo_animal1);
+    preenche(&novo_animal1);
     struct animais novo_animal2;
-    exibe(&novo_animal2);
+    preenche(&novo_animal2);
     struct animais novo_animal3;
-    exibe(&novo_animal3);
+    preenche(&novo_animal3);
     struct animais novo_animal4;
-    exibe(&novo_animal4);
+    preenche(&novo_animal4);
     struct animais novo_animal5;
-    exibe(&novo_animal5);
+    preenche(&novo_animal5);
     struct animais novo_animal6;
-    exibe(&novo_animal6);
-
+    preenche(&novo_animal6);
     return 0;
 }
