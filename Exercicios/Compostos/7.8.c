4d240220f7b8a80b3173330fd725869cc54e7605
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-05-22 12:14:45 -0300

    Update 7.8.c

--- Exercicios/Compostos/7.8.c
+++ Exercicios/Compostos/7.8.c
@@ -21,7 +21,7 @@ void preenchevet(int* x,int*y){
 
 void distante(int pt[]){
     int i ,x=0,y=0;
-    for(i=0;i<10;i+=2){
+    for(i=0;i<40;i+=2){
         if((pt[i] + pt[i+1]) >= x + y){
             x = pt[i];
             y = pt[i+1];
@@ -33,11 +33,11 @@ int main (void) {
     struct ponto novo_ponto;
     preenchestruct(&novo_ponto);
     printf("(%d,%d)\n",novo_ponto.x,novo_ponto.y);
-    int vet[5],i;
-    for(i=0;i<10;i+=2){
+    int vet[20],i;
+    for(i=0;i<40;i+=2){
         preenchevet(&vet[i],&vet[i+1]);
     }
-    for(i=0;i<10;i+=2){
+    for(i=0;i<40;i+=2){
         printf("(%d,%d)\n",vet[i],vet[i+1]);
     }
 
