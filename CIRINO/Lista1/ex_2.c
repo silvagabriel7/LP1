f2ba706fc2f0bc1b1113904a3a865cf1f59d0a28
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-09-06 16:38:35 -0300

    Create ex_2.c

---
+++ CIRINO/Lista1/ex_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int cont = 0;
+    int n = 0;
+    printf("Digite o número: ");
+    scanf("%d",&n);
+    for(int i = n;i >= 1;i --){
+        if(n % i == 0){
+            cont += 1;
+        }
+    }
+    if (cont == 2){
+        printf("O número %d é primo",n);
+    }else{
+        printf("O número %d não é primo",n);
+    }
+    return 0;
+}
