8b23c4f36de05b80b4148db3ea82abfe9a61e41f
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-09-06 16:40:44 -0300

    Create ex_11.c

---
+++ CIRINO/Lista1/ex_11.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <string.h>
+
+int main(void){
+    int len;
+    
+    char palavras[5][15] = {"banheira", "rato", "sol", "parede", "tudo"};
+    char vogais[10] = {"AaEeIiOoUu"};
+    for(int i = 0;i < 5;i++){
+        int cont = 0;
+        len = strlen(palavras[i]);
+        for(int j = 0; j < len;j++){
+            for(int k = 0; k < 10; k++){
+                if(palavras[i][j] == vogais[k]){
+                    cont += 1;
+                }
+            }
+                
+        }
+        if(cont >= 3){
+            printf("%s\n",palavras[i]);
+        }
+    }
+    return 0;
+}
+    
