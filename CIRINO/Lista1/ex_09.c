f6cdbcead7be7e400d162c6e58607c1a59e08e40
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-09-10 20:41:13 -0300

    Create ex_09.c

---
+++ CIRINO/Lista1/ex_09.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    int cont = 0;
+    char vet[80] = {"caracol"};
+    int total = strlen(vet);
+    for(int i = 0;i < total;i++){
+        for(int j = i+1; j < total;j++){
+            if(vet[i] == vet[j]){
+                vet[i] = 0;
+            }
+        }
+    }
+    for(int i = 0;i < total;i++){
+        if(vet[i] != 0){
+            cont += 1;
+        }
+    }
+    printf("%d\n",cont);
+    return 0;
+}
