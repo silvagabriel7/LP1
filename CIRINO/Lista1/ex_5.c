c148d361ef6e22cf5f0e6ce1a848938c59c2292d
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-09-10 10:59:42 -0300

    Update ex_5.c

--- CIRINO/Lista1/ex_5.c
+++ CIRINO/Lista1/ex_5.c
@@ -2,12 +2,28 @@
 
 int main(void){
     float v;
-    float r;
-    printf("Digite o valor desejado: ");
+    int m1=100, m2=50, m3=25, m4=10, m5=5, m6=1;
+    int nv;
+    printf("Digite o valor desejado:");
     scanf("%f",&v);
-    r = (v % 0.50);
-    printf("%.2f de moedas de 1\n",v);
-    printf("%.2f de moedas de 1: \n",r/100);
-    
+    nv = v*100;
+    printf("O troco em moedas será:\n");
+    v = nv / m1;
+    printf("%.0f de 1 real\n",v);
+    nv = nv % m1;
+    v = nv / m2;
+    printf("%.0f de 0.50 centavos\n",v);
+    nv = nv % m2;
+    v = nv / m3;
+    printf("%.0f de 0.25 centavos\n",v);
+    nv = nv % m3;
+    v = nv / m4;
+    printf("%.0f de 0.10 centavos\n",v);
+    nv = nv % m4;
+    v = nv / m5;
+    printf("%.0f de 0.05 centavos\n",v);
+    nv = nv % m5;
+    v = nv / m6;
+    printf("%.0f de 0.01 centavos\n",v);
     return 0;
 }
