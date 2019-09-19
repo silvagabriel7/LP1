bb2fc8cdd7a866a955d00e5cd2633cc48f00aed9
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-09-19 10:32:05 -0300

    Update ex_10.c

--- CIRINO/Lista1/ex_10.c
+++ CIRINO/Lista1/ex_10.c
@@ -2,37 +2,51 @@
 #include <string.h>
 
 int main(void){
-    char frase[80] = {"Esta kkk"};
+    char frase[80] = {"Esta frase eh bonita e a frase eh bela"};
     int tam;
     int n = 0;
-    char palavra[n][80];
-    int cont = 0;
     tam = strlen(frase);
-    
+    char palavra[tam][80];
+    int cont = 0;
+    int x;
 
-    for(int i = 0;i < tam;i ++){
+    printf("Esta frase eh bonita e a frase eh bela\n");
+
+    for(int i = 0; i < tam; i++){
         if(frase[i] == ' '){
+            x=0;
             for(int j = i-cont;j < i;j ++){
-                palavra[n][j] = frase[j];
-                printf("%c\n",palavra[n][j]); 
+                palavra[n][x] = frase[j];
+                x++;
             }
             n +=1;
             cont = 0;
         }else{
+
             if(frase[i+1] == '\0'){
+                x=0;
                 for(int k = i-cont;k < i+1;k ++){
-                    palavra[n][k] = frase[k];
-                    printf("%c\n",palavra[n][k]); 
+                    palavra[n][x] = frase[k];
+                    x++; 
                 }
                 n +=1;
-                cont += 1;
-            }else{
-                cont += 1;
             }
+            cont += 1;
         }
     }
+    int c=0;
     for(int i = 0;i < n;i++){
-        printf("%s\n",palavra[i]);
+        c+=1;
+        for(int j = i+1;j < n;j++){
+            if(strcmp(palavra[i],palavra[j])==0){
+                c+=1;
+                strcpy(palavra[j], " ");
+            }
+        }
+        if(strcmp(palavra[i]," ")!=0){
+            printf("%s:%d\n",palavra[i],c);
+        }
+        c=0;
     }
     return 0;
 }
