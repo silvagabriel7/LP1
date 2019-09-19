d014c7c5dfd8bbee990b3a499475f1199d9d1cb4
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-09-19 13:37:46 -0300

    consertar

--- CIRINO/Lista1/ex_07.c
+++ CIRINO/Lista1/ex_07.c
@@ -3,7 +3,9 @@
 struct Aluno{
     char nome[20];
     float nota;
+    float media;
 };
+int n =0;
 
 void preencher_aluno(struct Aluno* p){
     printf("Digite o nome do aluno: \n");
@@ -13,8 +15,13 @@ void preencher_aluno(struct Aluno* p){
 }
 
 void calcular_media(struct Aluno* p, float m){
+    
     if(p->nota > m){
         printf("%s com média %.1f\n",p->nome,p->nota);
+        if(n <= 5){
+            n+=1;
+        }
+        
     }
 }
 
@@ -22,7 +29,7 @@ int main(void){
     int cont = 0;
     float soma = 0;
     float media_t;
-    struct Aluno alunos[5];
+    struct Aluno alunos[10];
 
     for(int i =0; i < 5; i++){
         preencher_aluno(&alunos[i]);
@@ -35,10 +42,17 @@ int main(void){
 
     media_t = soma/cont;
     printf("Média da turma: %.1f\n",media_t);
-
-    for(int i = 0; i < 5; i++){
+    
+    for(int i = 0; i < 10; i++){
         calcular_media(&alunos[i], media_t);
     }
+    int maior = 0;
+    for(int i = 0; i < n; i++){
+        if(alunos[i].)
+    }
+
+
+
 
     return 0;
 }
