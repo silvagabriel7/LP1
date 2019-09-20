21741a925609b1014ac8bfc4751574d7d009828c
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-09-20 15:45:52 -0300

    Ok

--- CIRINO/Lista1/ex_14.c
+++ CIRINO/Lista1/ex_14.c
@@ -3,9 +3,7 @@
 int main(void){
     int seg = 00,min = 00,hr= 00;
     for(hr = 00;hr < 24;hr ++){
-        printf("%d:%d:%d\n",hr,min,seg);
         for(min = 00; min < 60;min ++){
-            printf("%d:%d:%d\n",hr,min,seg);
             for(seg = 00; seg < 60;seg ++){
                 printf("%d:%d:%d\n",hr,min,seg);
             }    
