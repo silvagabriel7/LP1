1bacda907ff6f96000cb8f15d9775f6453758b74
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-10-01 20:59:06 -0300

    Consertar

--- CIRINO/Lista1/ex_06.c
+++ CIRINO/Lista1/ex_06.c
@@ -6,20 +6,23 @@ int main(void){
         int qtdg;
         float vlg;
     };
+
     int n = 0;
     printf("Digite o número de paradas contando a saída e a chegada: \n");
     scanf("%d",&n);
+
     struct Parada p[n];
 
     float vlt = 0;
     int qtdgt = 0;
+
     for(int i = 1;i <= n; i ++){
         int tq = 50;
         printf("Na parada %d, quanto estava no odômetro: \n", i);
         scanf("%d",&p[i].od);
         printf("Na parada %d, quanto foi a qtd de gasolina: \n", i);
         scanf("%d",&p[i].qtdg);
-        printf("Na parada %d, quanto foi pago de gasolina : \nR$", i);
+        printf("Na parada %d, quanto foi pago de gasolina: \nR$", i);
         scanf("%f", &p[i].vlg);
         vlt += p[i].vlg;
         tq -= p[i].qtdg;
@@ -36,25 +39,33 @@ int main(void){
     }
     printf("=================================\n");
 
-    for(int i = 1;i <= n;i++){
+    // for(int i = 1;i <= n;i++){
+    //     int km = 0;
+    //     km = p[i+1].od - p[i].od;
+    //     int kml = 0;
+    //     kml = km/p[i+1].qtdg;
+    //     if(kml != 0){
+    //         printf("Quilometragem obtida por litro de gasolina entre cada parada %d e %d: %d\n",i,i+1,kml);
+    //     }
+    // }
 
-        int km = 0;
-        km = p[i+1].od - p[i].od;
-        int kml = 0;
-        kml = km/p[i+1].qtdg;
-        if(kml != 0){
-            printf("Quilometragem obtida por litro de gasolina entre cada parada %d e %d: %d\n",i,i+1,kml);
-        }
-    }
+
     int km = 0;
     km = p[n].od - p[1].od;
-    int kml = 0;
-    kml = km/qtdgt;
-    printf("Quilometragem obtida por litro de gasolina em toda a viagem: %d\n",kml);
+    printf("%d\n",km);
+    printf("%d\n",qtdgt);
 
-    float c = 0;
-    c = vlt/km;
-    printf("Custo do combustível por quilometro rodado em toda a viagem: %.2f\n",c);
+    // int kml = 0;
+    // kml = km/qtdgt;
+    // printf("Quilometragem obtida por litro de gasolina em toda a viagem: %d\n",kml);
+
+    // float c = 0;
+    // c = vlt/km;
+    // printf("Custo do combustível por quilometro rodado em toda a viagem: %.2f\n",c);
 
     return 0;
 }
+
+
+    
+    
