26080f7ccef4e02fa36b4ea12f582c0341b7d11e
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-10-02 16:32:12 -0300

    Ok

--- CIRINO/Lista1/ex_16.c
+++ CIRINO/Lista1/ex_16.c
@@ -7,6 +7,8 @@ int main(void){
     int j2[2];
     int d1 = 0;
     int d2 = 0;
+    int t1 = 0;
+    int t2 = 0;
     srand((unsigned)time(NULL));
     for(int i = 1;i < 12;i++){
         printf("RODADA %d\n",i);
@@ -27,16 +29,20 @@ int main(void){
 
         if((j1[0] == j1[1] && j2[0] != j2[1])){
             printf("JOGADOR 1 VENCEDOR\n");
+            t1 += 1;
         }else{
             if(j2[0] == j2[1] && j1[0] != j1[1]){
                 printf("JOGADOR 2 VENCEDOR\n");
+                t2 += 1;
             }else{
                 if(j2[0] == j2[1] && j1[0] == j1[1]){
                     if(d1 > d2){
                         printf("JOGADOR 1 VENCEDOR\n");
+                        t1 += 1;
                     }else{
                         if(d2 > d1){
                             printf("JOGADOR 2 VENCEDOR\n");
+                            t2 += 1;
                         }else{
                             printf("EMPATE\n");
                         }
@@ -44,9 +50,11 @@ int main(void){
                 }else{
                     if(d1 > d2){
                         printf("JOGADOR 1 VENCEDOR\n");
+                        t1 += 1;
                     }else{
                         if(d2 > d1){
                             printf("JOGADOR 2 VENCEDOR\n");
+                            t2 += 1;
                         }else{
                             printf("EMPATE\n");
                         }
@@ -55,10 +63,20 @@ int main(void){
                 }
             }
         }
-        
         d1 = 0;
         d2 = 0;
         printf("==================================\n");
+    }
+    printf("TOTAL DO JOGO\n");
+    printf("\n");
+    if(t1 > t2){
+        printf("O JOGADOR 1 FOI CAMPEÃO POR %d a %d\n",t1,t2);
+    }else{
+        if(t2 > t1){
+            printf("O JOGADOR 2 FOI CAMPEÃO POR %d a %d\n",t2,t1);
+        }else{
+            printf("DEU EMPATE POR %d a %d\n",t1,t2);
+        }
     }
     return 0;
 }
