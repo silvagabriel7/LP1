0bb65897c6cc8315de80bc2cb9b817a15f1682cd
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-10-02 15:00:32 -0300

    Ok

--- CIRINO/Lista1/ex_06.c
+++ CIRINO/Lista1/ex_06.c
@@ -17,7 +17,6 @@ int main(void){
     int qtdgt = 0;
 
     for(int i = 1;i <= n; i ++){
-        int tq = 50;
         printf("Na parada %d, quanto estava no odômetro: \n", i);
         scanf("%d",&p[i].od);
         printf("Na parada %d, quanto foi a qtd de gasolina: \n", i);
@@ -25,8 +24,7 @@ int main(void){
         printf("Na parada %d, quanto foi pago de gasolina: \nR$", i);
         scanf("%f", &p[i].vlg);
         vlt += p[i].vlg;
-        tq -= p[i].qtdg;
-        qtdgt += tq;
+        qtdgt += p[i].qtdg;
     }
 
 
@@ -39,33 +37,28 @@ int main(void){
     }
     printf("=================================\n");
 
-    // for(int i = 1;i <= n;i++){
-    //     int km = 0;
-    //     km = p[i+1].od - p[i].od;
-    //     int kml = 0;
-    //     kml = km/p[i+1].qtdg;
-    //     if(kml != 0){
-    //         printf("Quilometragem obtida por litro de gasolina entre cada parada %d e %d: %d\n",i,i+1,kml);
-    //     }
-    // }
+    for(int i = 1;i <= n;i++){
+        int km = 0;
+        km = p[i+1].od - p[i].od;
 
+        float kml = 0;
+        kml = km/p[i+1].qtdg;
+
+        if(kml > 0){
+            printf("Quilometragem obtida por litro de gasolina entre cada parada %d e %d: %.1f kms\n",i,i+1,kml);
+        }
+    }
 
     int km = 0;
     km = p[n].od - p[1].od;
-    printf("%d\n",km);
-    printf("%d\n",qtdgt);
 
-    // int kml = 0;
-    // kml = km/qtdgt;
-    // printf("Quilometragem obtida por litro de gasolina em toda a viagem: %d\n",kml);
+    float kml = 0;
+    kml = km/qtdgt;
+    printf("Quilometragem obtida por litro de gasolina em toda a viagem: %.1f kms\n",kml);
 
-    // float c = 0;
-    // c = vlt/km;
-    // printf("Custo do combustível por quilometro rodado em toda a viagem: %.2f\n",c);
+    float c = 0;
+    c = vlt/km;
+    printf("Custo do combustível por quilometro rodado em toda a viagem: R$%.2f\n",c);
 
     return 0;
 }
-
-
-    
-    
