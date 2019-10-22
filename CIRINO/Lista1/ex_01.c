38b3a6831d67e30dd6e2aea393e101428373613b
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-10-22 14:01:36 -0300

    Ok

--- CIRINO/Lista1/ex_01.c
+++ CIRINO/Lista1/ex_01.c
@@ -1,3 +1,8 @@
+/* Escreva um programa em C que encontre o menor inteiro positivo n que atenda as seguintes condições :
+    n / 3 = x inteiro e resto 2
+    n / 5 = y inteiro e resto 3
+    n / 7 = z inteiro e resto 4*/
+
 #include <stdio.h>
 
 int main(void){
