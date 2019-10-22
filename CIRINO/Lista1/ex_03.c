d9ce697889ba2db53d4bac76f81e9362c0e798b1
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-10-22 14:03:02 -0300

    Ok

--- CIRINO/Lista1/ex_03.c
+++ CIRINO/Lista1/ex_03.c
@@ -1,3 +1,8 @@
+/*Um inteiro perfeito n é igual a soma de todos os seus divisores próprios. A faixa de seus divisores
+próprios vai de 1 a (n-1). Por exemplo : 6 é um inteiro perfeito; a soma de todos os seus divisores próprios (
+1 + 2 + 3) é igual a 6. Escreva um programa em C que determine se um número no intervalo de 1 a 32767 é
+um inteiro perfeito.*/
+
 #include <stdio.h>
 
 int main(void){
