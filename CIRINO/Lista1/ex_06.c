7e0887dc9c9927f1f19a1d3e9e13917afa00d8e5
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-10-22 14:05:05 -0300

    Ok

--- CIRINO/Lista1/ex_06.c
+++ CIRINO/Lista1/ex_06.c
@@ -1,3 +1,13 @@
+/*Um motorista acaba de retornar de um feriado prolongado. Em cada parada de reabastecimento ele
+registrou a leitura de seu odômetro, a quantidade de gasolina comprada (suponha que ele tenha enchido o
+tanque cada vez) e o preço pago. Além disso, suponha também que ele tenha enchido o tanque antes de
+partir e imediatamente após retornar. Escreva um programa em C para ler, em primeiro lugar, o número total
+de reabastecimentos feitos (incluindo o primeiro e o último) e, a seguir, os dados relativos à compra de
+gasolina e calcular :
+    a) a quilometragem obtida por litro de gasolina entre cada par de paradas de reabastecimento;
+    b) a quilometragem obtida por litro de gasolina em toda a viagem
+    c) custo do combustível por quilometro rodado em toda a viagem*/
+
 #include <stdio.h>
 
 int main(void){
