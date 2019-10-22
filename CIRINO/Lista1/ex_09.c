750d35f548f3e71e8027c7d3c1b8a46a7ade5c45
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-10-22 14:07:14 -0300

    Ok

--- CIRINO/Lista1/ex_09.c
+++ CIRINO/Lista1/ex_09.c
@@ -1,3 +1,6 @@
+/*Escrever um programa em C que calcule e imprima o número de caracteres distintos presentes em uma
+cadeia de caracteres dada. Considerar o tamanho máximo da cadeia como sendo de 80 colunas.*/
+
 #include <stdio.h>
 #include <string.h>
 
