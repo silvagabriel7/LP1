de3136e23fba7e788e71214fe8337a88e6691600
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-10-22 14:10:18 -0300

    Ok

--- CIRINO/Lista1/ex_14.c
+++ CIRINO/Lista1/ex_14.c
@@ -1,3 +1,11 @@
+/*Faça um programa em C que simule um cronômetro com horas, minutos e segundos. A simulação
+deverá mostrar no vídeo o cronômetro no seguinte formato HH: MM: SS. Inicialmente o cronômetro será
+
+zerado ( 00: 00: 00 ), sendo que os segundos começarão a ser incrementados, depois os minutos e
+depois as horas. Lembre-se que a cada 60 segundos os minutos deverão ser incrementados, depois os
+minutos voltarão a zero. Quando as horas estiverem prestes a chegar em 25, o cronômetro deverá
+voltar a zero ( 00: 00: 00 ).*/
+
 #include <stdio.h>
 
 int main(void){
