6284de9393cfab24ee4f3b339ee71db4064c6022
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-11-25 16:43:40 -0300

    terminar

--- CIRINO/l2ex1.c
+++ CIRINO/l2ex1.c
@@ -1,41 +1,104 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
-#define DIM 106
 
-int main(void){
-    FILE *file;                     
-    char frase[DIM];
-    int i;
-    struct dicionario{
+#define DIM 1000
+
+struct dicionario{
         struct dicionario *ant, *prox;
-        char palavra [80];
-        int num;
+        char palavra[50];
+        int qtd;
     };
 
-    struct dicionario *controle, *aux;
+int main(void){
+    int i = 0;
+    int k = 0;
+    int cont = 0;
+    int qtd_palavra = 1;
 
-    file = fopen("./input.txt.txt","r"); 
-    if(file == NULL)
-        {
+    char texto [DIM];
+
+    FILE *file;
+    file = fopen("./Desktop/testesc/texto.txt","r");
+    if(file == NULL){
         printf("Nao foi possivel abrir o arquivo.\n");
         getchar();
         exit(0);
-        }
-
-    fgets(frase, DIM, file);
-    printf("%s \n", frase);
+    }
+    fgets(texto, DIM, file);
+    printf("%s \n", texto);
     fclose(file);
 
-    controle = aux = NULL;            
-    controle = (struct dicionario*) malloc(sizeof(struct dicionario)); 
-    aux = controle;               
-    controle->prox = controle->ant = NULL;
-    strcpy(controle->palavra, frase) ;
-    controle->num = 1;
-    fclose(file);      
-    printf("%s \n", controle->palavra);
-    system("pause");
+    while(texto[cont] != '\0'){
+        if(texto[cont] == ' '){
+            qtd_palavra ++;
+        }
+        cont++;
+    }
+    printf("qtd de palavras: %d\n",qtd_palavra);
+
+    struct dicionario *corr, *aux;
+
+    corr = aux = NULL;
+    corr = (struct dicionario*) malloc(sizeof(struct dicionario));
+    aux = corr;
+    corr->prox = corr->ant = NULL;
+
+
+
+
+
+
+
+
+    while(texto[i] != ' ' && texto[i] != '\0'){
+        corr->palavra[i]=texto[i];
+        i++;
+    }
+
+    corr->palavra[i] = '\0';
+    corr->qtd = 1;
+
+    if(texto[i] != '\0'){
+        for(int j = 1;j < qtd_palavra; j++){
+            corr = (struct dicionario*)malloc(sizeof(struct dicionario));
+            corr->prox = NULL;
+            corr->ant = aux;
+            aux->prox = corr;
+            aux = corr;
+
+            if(texto[i] == ' '){
+                corr->palavra[k] = '\0';
+                i ++;
+                k = 0;
+            }
+
+            while(texto[i] != ' ' && texto[i] != '\0'){
+                corr->palavra[k] = texto[i];
+                i ++;
+                k ++;
+            }
+
+            corr->qtd = 1;
+            corr->palavra[k] = '\0';
+        }
+    }
+
+    while (corr->ant != NULL){
+        corr = corr->ant;
+    }
+
+    aux = corr;
+    printf("palavra: %s \n", corr->palavra);
+    printf ("quantidade: %d \n",corr->qtd);
+
+    while (corr->prox!=NULL){
+        corr = corr->prox;
+        printf("palavra: %s \n", corr->palavra);
+        printf ("quantidade: %d \n",corr->qtd);
+        free (aux);
+        aux = corr;
+    }
+
     return 0;
     }
