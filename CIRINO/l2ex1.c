748c34d53a7020f9f2e47935cf6fd1b87ca71770
silvagabriel7 <48592526+silvagabriel7@users.noreply.github.com>
2019-11-26 18:36:17 -0300

    Update l2ex1.c

--- CIRINO/l2ex1.c
+++ CIRINO/l2ex1.c
@@ -1,93 +1,110 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 #define DIM 1000
 
-int contar_palavras(char t[]){
-    int cont = 0;
-    int qtd = 1;
-    while(t[cont] != '\0'){
-        if(t[cont] == ' '){
-            qtd ++;
-        }
-        cont++;
+
+void lista_de_palavras(){
+
+    char t[100];
+    FILE *arq;
+    arq = fopen("./Desktop/testesc/texto.txt","r");
+    if(arq == NULL){
+        printf("Nao foi possivel abrir o arquivo.\n");
+        getchar();
+        exit(0);
     }
-    return qtd;
+    fgets(t, DIM, arq);
+    fclose(arq);
+
+    int tam;
+    int n = 0;
+    tam = strlen(t);
+    char palavra[DIM][50];
+    int cont = 0;
+    int x;
+
+    for(int i = 0; i < tam; i++){
+        if(t[i] == ' '){
+            x=0;
+            for(int j = i-cont;j < i;j ++){
+                palavra[n][x] = t[j];
+                x++;
+            }
+            n +=1;
+            cont = 0;
+        }else{
+
+            if(t[i+1] == '\0'){
+                x=0;
+                for(int k = i-cont;k < i+1;k ++){
+                    palavra[n][x] = t[k];
+                    x++; 
+                }
+                n +=1;
+            }
+            cont += 1;
+        }
+    }
+
+    FILE *file;
+    file = fopen("./Desktop/testesc/aux.txt","w");
+    int c=0;
+    for(int i = 0;i < n;i++){
+        c+=1;
+        for(int j = i+1;j < n;j++){
+            if(strcmp(palavra[i],palavra[j])==0){
+                c+=1;
+                strcpy(palavra[j], " ");
+            }
+        }
+        if(strcmp(palavra[i]," ")!=0){
+            fprintf(file,"%s:%d",palavra[i],c);
+        }
+        c=0;
+    }
+    fclose(file);
 }
-
-
 
 void gerar_dicionario(char t[]){
     int i = 0;
     int j;
     int k = 0;
-    int qtd_palavra;
-    qtd_palavra = contar_palavras(t);
-    
+    int c;
+
     struct dicionario{
         char palavra[50];
         int qtd;
         struct dicionario *ant, *prox;
     };
-
     struct dicionario *corr, *aux;
     corr = aux = NULL;
     corr = (struct dicionario*) malloc(sizeof(struct dicionario));
     aux = corr;
     corr->prox = corr->ant = NULL;
 
-    while(t[i] != ' ' && t[i] != '\0'){
-        corr->palavra[i]=t[i];
-        i++;
-    }
-
-    corr->palavra[i] = '\0';
-    corr->qtd = 1;
-
-    if(t[i] != '\0'){
-        for(int j = 1;j < qtd_palavra; j++){
-            corr = (struct dicionario*)malloc(sizeof(struct dicionario));
-            corr->prox = NULL;
-            corr->ant = aux;
-            aux->prox = corr;
-            aux = corr;
-
-            if(t[i] == ' '){
-                i ++;
-                k = 0;
-            }
-
-            while(t[i] != ' ' && t[i] != '\0'){
-                corr->palavra[k] = t[i];
-                i ++;
-                k ++;
-            }
-
-            corr->qtd = 1;
-            corr->palavra[k] = '\0';
-            k = 0;
+    for(int i=0;i < strlen(t);i++){
+        corr = (struct dicionario*)malloc(sizeof(struct dicionario));
+        corr->prox = NULL;
+        corr->ant = aux;
+        aux->prox = corr;
+        aux = corr;
+        while(t[i] != ':'){
+            corr->palavra[i]=t[i];
         }
+        corr->palavra[i] = '\0';
+        i+=2;
+        corr->qtd = t[i];
     }
-
-
-
     while (corr->ant != NULL){
         corr = corr->ant;
     }
 
-    aux = corr;
-    printf("palavra: %s \n", corr->palavra);
-    printf ("quantidade: %d \n",corr->qtd);
-
     FILE *arq;
-    arq = fopen("./dicionario.txt","w");
-    
-    
-
+    arq = fopen("./Desktop/testesc/dicionario.txt","w");
     while (corr->prox!=NULL){
         corr = corr->prox;
-        fwrite(corr, sizeof(struct dicionario),1,arq);
+        fprintf(arq,"%s: %d\n",corr->palavra,corr->qtd);
         free (aux);
         aux = corr;
     }
@@ -95,26 +112,18 @@ void gerar_dicionario(char t[]){
     
 }
 
-
-
-
-
 int main(void){
-    char texto[DIM];
+    lista_de_palavras();
+    char texto[1000];
     FILE *arq;
-    arq = fopen("./texto.txt","r");
+    arq = fopen("./Desktop/testesc/aux.txt","r");
     if(arq == NULL){
         printf("Nao foi possivel abrir o arquivo.\n");
         getchar();
         exit(0);
     }
     fgets(texto, DIM, arq);
-    printf("%s \n", texto);
     fclose(arq);
-
-
     gerar_dicionario(texto);
     return 0;
-    }
-
-
+}
